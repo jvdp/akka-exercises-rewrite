1e58427217dd91bc49a302e60bb6a73c23006c61
Jaap van der Plas <jaap.vdplas@lunatech.nl>
2021-09-03 18:28:03 +0200

    add Barista flow + tests, use compat flowActor for compatible Actor tests

--- exercises/src/main/scala/com/lightbend/training/coffeehouse/Barista.scala
+++ exercises/src/main/scala/com/lightbend/training/coffeehouse/Barista.scala
@@ -7,6 +7,14 @@ import akka.actor.{Actor, ActorRef, Props, Stash, Timers}
 
 import scala.concurrent.duration.FiniteDuration
 import scala.util.Random
+import akka.stream.scaladsl.Flow
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.Source
+import akka.stream.OverflowStrategy
+import akka.stream.stage.GraphStage
+import akka.stream.stage.GraphStageLogic
+import akka.stream.scaladsl.Sink
 
 object Barista {
 
@@ -15,6 +23,40 @@ object Barista {
 
   def props(prepareCoffeeDuration: FiniteDuration, accuracy: Int): Props =
     Props(new Barista(prepareCoffeeDuration, accuracy))
+
+  // *** streams ***
+
+  def flow[K](
+      prepareCoffeeDuration: FiniteDuration,
+      accuracy: Int
+  ): Flow[(K, PrepareCoffee), (K, CoffeePrepared), NotUsed] =
+    Flow[(K, PrepareCoffee)]
+      .map { case (key, PrepareCoffee(requested, guest)) =>
+        val coffee = if (Random.nextInt(100) < accuracy) requested else Coffee.anyOther(requested)
+        key -> CoffeePrepared(coffee, guest)
+      }
+      .delay(prepareCoffeeDuration)
+
+  // *** compat ***
+
+  def flowActor(
+      baristaFlow: Flow[(ActorRef, PrepareCoffee), (ActorRef, CoffeePrepared), NotUsed]
+  )(implicit system: ActorSystem): ActorRef =
+    system.actorOf(Props(new Actor {
+      val ref = Source
+        .actorRef[(ActorRef, PrepareCoffee)](
+          completionMatcher = PartialFunction.empty,
+          failureMatcher = PartialFunction.empty,
+          bufferSize = 0,
+          overflowStrategy = OverflowStrategy.fail
+        )
+        .via(baristaFlow)
+        .to(Sink.foreach { case (waiter, coffeePrepared) => waiter ! coffeePrepared })
+        .run()
+
+      override def receive: Receive = { case msg => ref ! (sender(), msg) }
+    }))
+
 }
 
 class Barista(prepareCoffeeDuration: FiniteDuration, accuracy: Int)
--- exercises/src/test/scala/com/lightbend/training/coffeehouse/BaristaSpec.scala
+++ exercises/src/test/scala/com/lightbend/training/coffeehouse/BaristaSpec.scala
@@ -5,14 +5,18 @@ package com.lightbend.training.coffeehouse
 
 import akka.testkit.TestProbe
 import scala.concurrent.duration.DurationInt
+import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.Sink
 
 class BaristaSpec extends BaseAkkaSpec {
 
+  // *** actor ***
   "Sending PrepareCoffee to Barista" should {
     "result in sending a CoffeePrepared response after prepareCoffeeDuration" in {
       val sender = TestProbe()
       implicit val ref = sender.ref
-      val barista = system.actorOf(Barista.props(100 milliseconds, 100))
+      // val barista = system.actorOf(Barista.props(100 milliseconds, 100))
+      val barista = Barista.flowActor(Barista.flow(100 milliseconds, 100))
       sender.within(50 milliseconds, 200 milliseconds) { // The time is not extremely accurate, so we relax the timing constraints.
         barista ! Barista.PrepareCoffee(Coffee.Akkaccino, system.deadLetters)
         sender.expectMsg(Barista.CoffeePrepared(Coffee.Akkaccino, system.deadLetters))
@@ -22,7 +26,8 @@ class BaristaSpec extends BaseAkkaSpec {
       val waiter = TestProbe()
       val accuracy = 50
       val runs = 1000
-      val barista = system.actorOf(Barista.props(0 milliseconds, accuracy))
+      // val barista = system.actorOf(Barista.props(0 milliseconds, accuracy))
+      val barista = Barista.flowActor(Barista.flow(0 milliseconds, accuracy))
       val guest = system.deadLetters
       var coffees = List.empty[Coffee]
       for (_ <- 1 to runs) {
@@ -35,6 +40,37 @@ class BaristaSpec extends BaseAkkaSpec {
       val expectedCount = runs * accuracy / 100
       val variation = expectedCount / 5
       coffees count (_ == Coffee.Akkaccino) shouldEqual expectedCount +- variation
+    }
+  }
+
+  "Barista.flow" should {
+    "result in sending a CoffeePrepared response after prepareCoffeeDuration" in {
+      val ref = TestProbe().ref
+
+      Source
+        .single(() -> Barista.PrepareCoffee(Coffee.Akkaccino, ref))
+        .via(Barista.flow(100 milliseconds, 100))
+        .runWith(Sink.seq)
+        .futureValue shouldEqual
+        Seq(
+          () -> Barista.CoffeePrepared(Coffee.Akkaccino, ref)
+        )
+    }
+
+    "result in sending a CoffeePrepared response with a random Coffee for an inaccurate one" in {
+      val ref = TestProbe().ref
+      val accuracy = 50
+      val runs = 1000
+      val expectedCount = runs * accuracy / 100
+      val variation = expectedCount / 5
+
+      Source
+        .repeat(() -> Barista.PrepareCoffee(Coffee.Akkaccino, ref))
+        .take(runs)
+        .via(Barista.flow(0 milliseconds, accuracy))
+        .runWith(Sink.seq)
+        .futureValue
+        .count(_._2.coffee == Coffee.Akkaccino) shouldEqual expectedCount +- variation
     }
   }
 }
